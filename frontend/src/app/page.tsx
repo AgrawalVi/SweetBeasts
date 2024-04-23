d1f85911f6e73435d40ff6bb02d8ebd168d3ee5f
Vishrut Agrawal <zvishrut@gmail.com>
2024-04-23 14:09:42 -0500

    initial commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import MacbookScrollDemo from "@/components/aceternity/MacbookScrollDemo";
+import { Combobox } from "@/components/ui/combobox";
+export default function Home() {
+  return (
+    <>
+      <MacbookScrollDemo></MacbookScrollDemo>
+      <Combobox></Combobox>
+    </>
+  );
+}
