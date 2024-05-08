855ed80fbf11dbb503e02b071458c79d2f94b836
Vishrut Agrawal <zvishrut@gmail.com>
2024-05-08 03:36:11 -0500

    moved out of frontend folder

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import MacbookScrollDemo from "@/components/aceternity/MacbookScrollDemo";
+import { TracingBeam } from "@/components/ui/tracing-beam";
+
+export default function Home() {
+  return (
+    <main>
+        <MacbookScrollDemo></MacbookScrollDemo>
+    </main>
+  );
+}
---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { MenuIcon } from "lucide-react"
+
+export function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <>
+      <div
+        className="relative w-screen z-50 hidden md:block"
+      >
+        <Menu setActive={setActive}>
+          <MenuItem setActive={setActive} active={active} item="Services">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/web-dev">Web Development</HoveredLink>
+              <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+              <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+              <HoveredLink href="/branding">Branding</HoveredLink>
+            </div>
+          </MenuItem>
+          <MenuItem setActive={setActive} active={active} item="Products">
+            <div className="text-sm grid grid-cols-2 gap-10 p-4">
+              <ProductItem
+                title="Pogo"
+                href="/products/pogo"
+                src="/pogo.jpg"
+                description="The Peachy Penguin."
+              />
+              <ProductItem
+                title="Pineapple Parrot"
+                href="https://tailwindmasterkit.com"
+                src="/pineapple-parrot.jpg"
+                description="The Pineapple Parrot"
+              />
+              <ProductItem
+                title="Tangerine Turtle"
+                href="https://gomoonbeam.com"
+                src="/tangerine-turtle.jpg"
+                description="The Tangerine Turtle."
+              />
+              <ProductItem
+                title="Lemon Lion"
+                href="https://userogue.com"
+                src="/lemon-lion.jpg"
+                description="The Lemon Lion."
+              />
+            </div>
+          </MenuItem>
+          <MenuItem setActive={setActive} active={active} item="Pricing">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/hobby">Hobby</HoveredLink>
+              <HoveredLink href="/individual">Individual</HoveredLink>
+              <HoveredLink href="/team">Team</HoveredLink>
+              <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+            </div>
+          </MenuItem>
+          <div className="absolute right-5">
+            <ModeToggle></ModeToggle>
+          </div>
+        </Menu>
+      </div>
+      <div className="block md:hidden h-20 bg-white dark:bg-black w-screen border">
+        <div className="flex justify-end">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon" className="m-5 ml-5">
+                <MenuIcon className="h-6 w-6" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              Hello
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+
+    </>
+
+  );
+}
