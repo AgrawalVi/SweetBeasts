5a496b13c286adcd9109c55b05749abee66adbbe
Vishrut Agrawal <zvishrut@gmail.com>
2024-05-16 19:30:09 -0500

    Created exit animation in navbar :)

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
 
-const transition = {
+const transitionIn = {
   type: "spring",
   mass: 0.5,
   damping: 11.5,
@@ -13,19 +13,28 @@ const transition = {
   restSpeed: 0.001,
 };
 
+const transitionOut = {
+  type: "spring",
+  mass: 0.7,
+};
+
 export const MenuItem = ({
   setActive,
   active,
+  lastActive,
+  setLastActive,
   item,
   children,
 }: {
   setActive: (item: string) => void;
   active: string | null;
+  lastActive: string | null;
+  setLastActive: (item: string) => void;
   item: string;
   children?: React.ReactNode;
 }) => {
   return (
-    <div onMouseEnter={() => setActive(item)} className="relative ">
+    <div onMouseEnter={() => setActive(item)} className="relative">
       <motion.p
         transition={{ duration: 0.3 }}
         className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white"
@@ -36,12 +45,12 @@ export const MenuItem = ({
         <motion.div
           initial={{ opacity: 0, scale: 0.85, y: 10 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
-          transition={transition}
+          transition={transitionIn}
         >
           {active === item && (
             <div className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
               <motion.div
-                transition={transition}
+                transition={transitionIn}
                 layoutId="active" // layoutId ensures smooth animation
                 className="bg-white dark:bg-black backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl"
               >
@@ -56,20 +65,49 @@ export const MenuItem = ({
           )}
         </motion.div>
       )}
+      { active === null && lastActive === item ? (
+        <motion.div
+          initial={{opacity: 1, scale: 1, y: 0}}
+          animate={{opacity: 0, scale: 0.85, y: 20}}
+          transition={transitionOut}
+        >
+          <div className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
+            <motion.div
+              transition={transitionOut}
+              layoutId="active" // layoutId ensures smooth animation
+              className="bg-white dark:bg-black backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl"
+            >
+              <motion.div
+                layout // layout ensures smooth animation
+                className="w-max h-full p-4"
+              >
+                {children}
+              </motion.div>
+            </motion.div>
+          </div>
+        </motion.div>
+      ) : null}
     </div>
   );
 };
 
 export const Menu = ({
+  active,
   setActive,
+  setLastActive,
   children,
 }: {
+  active: string | null,
   setActive: (item: string | null) => void;
+  setLastActive: (item: string | null) => void;
   children: React.ReactNode;
 }) => {
   return (
     <nav
-      onMouseLeave={() => setActive(null)} // resets the state
+      onMouseLeave={() => {
+        setLastActive(active);
+        setActive(null)
+      }}
       className="relative border-b-1 border-black/[0.1] dark:bg-black dark:border-white bg-white shadow-input flex justify-center items-center space-x-4 px-8 py-6 "
     >
       {children}
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -9,13 +9,14 @@ import { MenuIcon } from "lucide-react"
 
 export function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
+  const [lastActive, setLastActive] = useState<string | null>(null);
   return (
     <>
       <div
         className="relative w-screen z-50 hidden md:block"
       >
-        <Menu setActive={setActive}>
-          <MenuItem setActive={setActive} active={active} item="Services">
+        <Menu setActive={setActive} setLastActive={setLastActive} active={active}>
+          <MenuItem setActive={setActive} active={active} item="Services" setLastActive={setLastActive} lastActive={lastActive}>
             <div className="flex flex-col space-y-4 text-sm">
               <HoveredLink href="/web-dev">Web Development</HoveredLink>
               <HoveredLink href="/interface-design">Interface Design</HoveredLink>
@@ -23,7 +24,7 @@ export function Navbar({ className }: { className?: string }) {
               <HoveredLink href="/branding">Branding</HoveredLink>
             </div>
           </MenuItem>
-          <MenuItem setActive={setActive} active={active} item="Products">
+          <MenuItem setActive={setActive} active={active} item="Products" setLastActive={setLastActive} lastActive={lastActive}>
             <div className="text-sm grid grid-cols-2 gap-10 p-4">
               <ProductItem
                 title="Pogo"
@@ -51,7 +52,7 @@ export function Navbar({ className }: { className?: string }) {
               />
             </div>
           </MenuItem>
-          <MenuItem setActive={setActive} active={active} item="Pricing">
+          <MenuItem setActive={setActive} active={active} item="Pricing" setLastActive={setLastActive} lastActive={lastActive}>
             <div className="flex flex-col space-y-4 text-sm">
               <HoveredLink href="/hobby">Hobby</HoveredLink>
               <HoveredLink href="/individual">Individual</HoveredLink>
