a5a87b5c9ee0dfb40a8cb29328b364dacc28e568
Pranav Gogineni <pranavgv@gmail.com>
2024-05-18 12:13:40 -0500

    Makes card hovering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,29 +8,36 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import Image  from 'next/image'
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex items-end justify-start min-h-screen p-4 bg-gray-900">
-      <Card style={{ backgroundColor: '#F08080' }} className="w-64 shadow-lg rounded-lg shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)]">
-        <CardHeader className="p-4 border-b border-gray-200">
-          <CardTitle className="text-xl font-bold">Pogo</CardTitle>
-          <CardDescription className="text-gray-500">The Peachy Penguin</CardDescription>
-        </CardHeader>
-        <CardContent className="p-4 ">
-          < Image 
-            src="/pogo.jpg" 
-            width={100}
-            height={100}
-            alt="Pogo"
-          />
-        </CardContent>
-        <CardFooter className="p-4 border-t border-gray-200">
-          <p>Buy Now</p>
-        </CardFooter>
-      </Card>
+    <main className="flex flex-col items-start justify-start min-h-screen p-4 bg-[hsl(var(--background))] gap-4">
+      <CardContainer className="w-64 shadow-lg rounded-lg bg-[#F08080] shadow-[0_10px_20px_rgba(240,_46,_170,_0.7)]">
+        <CardBody className="relative group/card w-full h-auto rounded-xl p-4 border border-gray-200 bg-[#F08080]">
+          <CardItem translateZ="50" className="text-xl font-bold text-neutral-600 dark:text-white">
+            Make things float in air
+          </CardItem>
+          <CardItem as="p" translateZ="60" className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
+            Hover over this card to unleash the power of CSS perspective
+          </CardItem>
+          <CardItem translateZ="100" className="w-full mt-4 flex justify-center">
+            <Image
+              src="/pogo.jpg"
+              width={150}
+              height={150}
+              className="object-cover rounded-xl"
+              alt="thumbnail"
+            />
+          </CardItem>
+          <div className="flex justify-between items-center mt-4">
+            <CardItem translateZ={20} as="button" className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
+              Buy Now
+            </CardItem>
+          </div>
+        </CardBody>
+      </CardContainer>
     </main>
   );
 }
-
