afbba1f1d91ae6c480de8bc3a80fe097e7480340
Pranav Gogineni <pranavgv@gmail.com>
2024-05-19 21:38:31 -0500

    Adds infinite cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 'use client';
 
-import { CardBody, CardContainer, CardItem } from "@/components/aceternity/3d-card";
-import Image from 'next/image';
+
 import MailingComponent from "@/components/mailinglist.tsx/mailing";
 import ProductCards from "@/components/products/product-cards";
+import { InfiniteCards } from "@/components/reviews/scrollingreviews";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-start justify-start min-h-screen p-4 bg-[hsl(var(--background))] gap-4">
       <ProductCards />
-
+      <InfiniteCards />
       <MailingComponent />
-      
     </main>
   );
 }
