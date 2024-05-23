181cb742ed75629fd8b0dc308a415322a66024ff
Pranav Gogineni <pranav@macbook-pro.lan>
2024-05-22 23:17:14 -0500

    Adds a second card for the product page

--- src/app/products/pogo/page.tsx
+++ src/app/products/pogo/page.tsx
@@ -19,10 +19,20 @@ import { Button } from "@/components/ui/button";
 
 export default function Pogo() {
   return (
-    <div className="container mx-auto flex justify-end">
-      <Card className="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 h-96 lg:h-[500px]">
-        </Card>
-      <Card className="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 h-96 lg:h-[500px]">
+    <div className="container mx-auto flex justify-end space-x-4">
+      <Card className="w-full md:w-3/4 lg:w-[47%] xl:w-[32%] h-96 lg:h-[600px]">
+        <CardHeader className="mb-8">
+          <CardTitle className="text-center">Pogo the Peachy Penguin</CardTitle>
+          <CardDescription className="text-center">#1 Best Seller</CardDescription>
+        </CardHeader>
+        <CardContent className="mb-48">
+          <p className="mb-5">Pogo is as charming as they come, with a soft, peach-colored belly that makes cuddling irresistible.</p>
+          <p className="mb-5">Crafted with high-quality materials, Pogo the Peachy Penguin is designed to be both durable and huggable. His plush exterior is incredibly soft to the touch, ensuring endless snuggles and comfort.</p>
+          <p className="mb-5">Pogo is the perfect gift for any occasion, whether it's a birthday, holiday, or just because. He's sure to bring a smile to anyone's face!</p>
+          <p>Add a splash of peachy fun to your life with Pogo the Peachy Penguin. He’s waiting to slide into your home and bring joy, laughter, and a whole lot of love.</p>
+        </CardContent>
+      </Card>
+      <Card className="w-full md:w-3/4 lg:w-[47%] xl:w-[32%] h-96 lg:h-[500px]">
         <CardHeader className="mb-8">
           <CardTitle>Buy Now</CardTitle>
           <CardDescription>15.99</CardDescription>
@@ -31,21 +41,21 @@ export default function Pogo() {
           <p>Want it Now? Order before it runs out!</p>
         </CardContent>
         <div className="mb-8 flex justify-center">
-        <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Quantity" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Quantity</SelectLabel>
-          <SelectItem value="1">1</SelectItem>
-          <SelectItem value="2">2</SelectItem>
-          <SelectItem value="3">3</SelectItem>
-          <SelectItem value="4">4</SelectItem>
-          <SelectItem value="5">5</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Quantity" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Quantity</SelectLabel>
+                <SelectItem value="1">1</SelectItem>
+                <SelectItem value="2">2</SelectItem>
+                <SelectItem value="3">3</SelectItem>
+                <SelectItem value="4">4</SelectItem>
+                <SelectItem value="5">5</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
         <CardFooter className="flex justify-center">
           <Button>Buy Now</Button>
