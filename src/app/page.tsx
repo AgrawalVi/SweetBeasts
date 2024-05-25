4b3f2603136c741f7d84f86b6b6afe266cff5acb
Vishrut Agrawal <zvishrut@gmail.com>
2024-05-25 13:25:32 -0500

    fixed issue with h-dvh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <main
       className={cn(
-        "h-dvf w-screen bg-neutral-950 relative flex flex-col items-center justify-center antialiased",
+        "min-h-dvh w-screen bg-neutral-950 relative flex flex-col items-center justify-center antialiased",
         josefinSans.className
       )}
     >
