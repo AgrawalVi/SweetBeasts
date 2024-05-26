c4b906e18b6d086d54ba9bbb373fe2b43eb47ce6
Vishrut Agrawal <zvishrut@gmail.com>
2024-05-26 14:04:10 -0500

    added placeholders for who we are and our story sections

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,17 +3,31 @@
 import SweetCards from "@/components/about-us/sweet-cards"
 import { TextGenerateEffect } from "@/components/aceternity/text-generate-effect"
 import MissionStatementSection from "@/components/about-us/mission-statement"
-import { ModeToggle } from "@/components/ui/mode-toggle"
+import TextSection from "@/components/custom/text-section"
 
 export default function CanvasRevealEffectDemo() {
   return (
     <main className="relative dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
       <div className="pb-20 pt-10 relative space-y-10">
-        <div>
+        <div className="flex flex-col place-items-center">
           <h1 className="text-5xl md:text-7xl text-center bg-clip-text text-transparent header-gradient from-cyan-500 to-cyan-900 drop-shadow-md bg-opacity-50 font-coiny">
             OUR STORY
           </h1>
+          <TextSection text='At SweetBeasts, we craft delightful plushies that blend the charm of
+            fruits with the joy of animals, inspiring healthier lifestyles and a
+            spirit of compassion. By merging fun with education, each creation
+            serves as a playful advocate for a balanced diet, enhancing both
+            physical and emotional well-being. Our plushies are not only
+            designed to entertain but also to cultivate empathy, offering
+            comfort and companionship to individuals of all ages. Committed to
+            meaningful engagement, we foster a dynamic community that shapes our
+            initiatives and deepens the impact of our contributions. We are
+            dedicated to supporting a variety of hunger and food-related
+            charities and providing our plushies as comfort items to children in
+            need including those in hospitals, shelters, and other similar
+            environments. Each purchase is a step towards building a more
+            caring, connected, and healthier world.'/>
         </div>
         <MissionStatementSection />
         <div>
@@ -27,10 +41,24 @@ export default function CanvasRevealEffectDemo() {
           />
           <SweetCards />
         </div>
-        <div>
+        <div className="flex flex-col place-items-center">
           <h1 className="text-5xl md:text-7xl text-center bg-clip-text text-transparent header-gradient from-cyan-500 to-cyan-900 drop-shadow-md bg-opacity-50 font-coiny">
             WHO WE ARE
           </h1>
+          <TextSection text='At SweetBeasts, we craft delightful plushies that blend the charm of
+            fruits with the joy of animals, inspiring healthier lifestyles and a
+            spirit of compassion. By merging fun with education, each creation
+            serves as a playful advocate for a balanced diet, enhancing both
+            physical and emotional well-being. Our plushies are not only
+            designed to entertain but also to cultivate empathy, offering
+            comfort and companionship to individuals of all ages. Committed to
+            meaningful engagement, we foster a dynamic community that shapes our
+            initiatives and deepens the impact of our contributions. We are
+            dedicated to supporting a variety of hunger and food-related
+            charities and providing our plushies as comfort items to children in
+            need including those in hospitals, shelters, and other similar
+            environments. Each purchase is a step towards building a more
+            caring, connected, and healthier world.'/>
         </div>
       </div>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,8 +72,11 @@ const config = {
       },
       boxShadow: {
         input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
-        pink: 'shadow-[0px_0px_30px_10px_#ed64a6]',
-        teal: 'shadow-[0px_0px_30px_10px_#319795]'
+        'study-desk': '0px 4px 16px rgba(17, 17, 26, 0.1), 0px 8px 24px rgba(17, 17, 26, 0.1), 0px 16px 56px rgba(17, 17, 26, 0.1)',
+        'teal-light': '0px 0px 20px 0px #319795',
+        'pink-light': '0px 0px 20px 0px #ed64a6',
+        'pink-strong': '0px 0px 30px 10px #ed64a6',
+        'teal-strong': '0px 0px 30px 10px #319795',
       },
     },
   },
