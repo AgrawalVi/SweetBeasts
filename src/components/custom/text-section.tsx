5b6141e6109d16970a35aef99bb27d41e542eb21
Vishrut Agrawal <zvishrut@gmail.com>
2024-05-26 14:24:42 -0500

    changes to text color on cards and shadow

--- src/components/custom/text-section.tsx
+++ src/components/custom/text-section.tsx
@@ -9,7 +9,7 @@ const TextSection = ({
 }) => {
   return (
     <>
-      <div className={cn("bg-background rounded-xl max-w-[60rem] text-base md:text-lg text-center p-5 sm:p-10 m-10 shadow-teal-light dark:shadow-pink-light hover:shadow-teal-strong dark:hover:shadow-pink-strong transition-all hover:scale-[1.01] duration-1000", className)}>
+      <div className={cn("bg-background rounded-xl max-w-[60rem] text-base md:text-lg text-center p-5 sm:p-10 m-10 shadow-pink-light dark:shadow-teal-light hover:shadow-pink-strong dark:hover:shadow-teal-strong transition-all hover:scale-[1.03] duration-1000", className)}>
         {text}
       </div>
     </>
