a24d93a7a63989b4ac1e4242910ee1d169fd59ee
Vishrut Agrawal <vagrawal24@wisc.edu>
2024-05-27 14:26:39 -0500

    fixed issue where mode toggle needs to be clicked twice first time around

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -17,19 +17,19 @@ export function ModeToggle({ className }: { className?: string }) {
 
   return (
     <>
-      {!isMounted ? (
-        <Button variant="outline" size="icon" className={className} />
-      ) : (
-        <Button
-          variant="outline"
-          size="icon"
-          className={className}
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        </Button>
-      )}
+      <Button
+        variant="outline"
+        size="icon"
+        className={className}
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      >
+        {isMounted && (
+          <>
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          </>
+        )}
+      </Button>
     </>
   )
 }
