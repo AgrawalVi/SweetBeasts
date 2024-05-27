af38e729af2dc47530630d201e3b9ac3ba854d38
Vishrut Agrawal <zvishrut@gmail.com>
2024-05-26 23:51:50 -0500

    added auth files from sweetbeasts admin

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -5,3 +5,78 @@ export const JoinEmailListSchema = z.object({
     message: 'Please enter a valid email address'
   })
 });
+
+export const LoginSchema = z.object({
+  email: z
+    .string()
+    .min(1, {
+      message: "Email is required",
+    })
+    .email({
+      message: "Please enter a valid email address",
+    }),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
+  code: z.optional(z.string()),
+})
+
+export const RegisterSchema = z
+  .object({
+    email: z
+      .string()
+      .min(1, {
+        message: "Email is required",
+      })
+      .email({
+        message: "Please enter a valid email address",
+      }),
+    password: z.string().min(8, {
+      message: "Minimum 8 characters required",
+    }),
+    confirmPassword: z.string().min(8, {
+      message: "Minimum 8 characters required",
+    }),
+    name: z.string().min(1, {
+      message: "Name is required",
+    }),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.confirmPassword
+    },
+    {
+      message: "Passwords do not match!",
+      path: ["confirmPassword"],
+    }
+  )
+
+export const ResetPasswordSchema = z.object({
+  email: z
+    .string()
+    .min(1, {
+      message: "Email is required",
+    })
+    .email({
+      message: "Please enter a valid email address",
+    }),
+})
+
+export const NewPasswordSchema = z
+  .object({
+    password: z.string().min(8, {
+      message: "Minimum 8 characters required",
+    }),
+    confirmPassword: z.string().min(8, {
+      message: "Minimum 8 characters required",
+    }),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.confirmPassword
+    },
+    {
+      message: "Passwords do not match!",
+      path: ["confirmPassword"],
+    }
+  )
