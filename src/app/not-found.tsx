7c46b3aeef8bc1febd50d8fde6e81948a2fb3403
Vishrut Agrawal <zvishrut@gmail.com>
2024-05-27 18:45:18 -0500

    added not-found file

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <div className='flex flex-col w-full items-center h-full flex-grow justify-center space-y-4'>
+      <h2 className='text-2xl '>404 - PAGE NOT FOUND</h2>
+      <p>This page does not exist</p>
+      <Link href="/"><Button>Return Home</Button></Link>
+    </div>
+  )
+}
