c2945e2672c8e0022ced408fdbdf9a7ff6511f89
Vishrut Agrawal <zvishrut@gmail.com>
2024-05-28 18:22:40 -0500

    working on stripe API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 'use client';
 
-import ProductCards from "@/components/products/product-cards";
+import { createPaymentIntent } from '@/actions/stripe/payment-intent';
+import React from 'react'
 
 export default function Home() {
+
+  async function createPayment() {
+    createPaymentIntent(500, "usd").then(response => console.log(response)).catch(error => console.error(error))
+  }
+
   return (
     <main className="flex flex-col items-start justify-start min-h-screen p-4 bg-[hsl(var(--background))] gap-4">
-      <ProductCards />
-      {/* <MailingComponent /> */}
+      <button onClick={() => {createPayment()}}>
+        create payment intent
+      </button>
     </main>
   );
 }
---
+++ src/lib/stripe.ts
@@ -0,0 +1,23 @@
+import Stripe from "stripe"
+
+const API_KEY = process.env.STRIPE_SECRET_KEY
+
+if (!API_KEY) {
+  console.error("STRIPE API KEY UNAVAILABLE")
+  process.exit(1)
+}
+
+const stripe = new Stripe(API_KEY)
+
+export async function createPaymentIntent(amount: number, currency: string) {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: currency,
+    })
+
+    return paymentIntent
+  } catch (e) {
+    console.error("Error creating payment intent", e)
+  }
+}
