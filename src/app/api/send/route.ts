b24bde0d85cc1f6d6f0617fd9089f0ad1e449b39
Pranav Gogineni <pranavgv@gmail.com>
2024-05-30 15:39:24 -0500

    adds resend email

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,6 @@
+import { EmailTemplate } from '../../../components/email-template';
+import { Resend } from 'resend';
+import * as React from 'react';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
