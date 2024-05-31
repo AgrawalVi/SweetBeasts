8e006f286a585d676f7fe1dafade0b4d518e133a
Vishrut Agrawal <zvishrut@gmail.com>
2024-05-30 20:34:10 -0500

    changed page.tsx

--- src/app/(floating-nav-pages)/page.tsx
+++ src/app/(floating-nav-pages)/page.tsx
@@ -1,23 +1,13 @@
 'use client';
 
-import { createPaymentIntent } from '@/actions/stripe/payment-intent';
-import React from 'react'
-import { useCurrentUser } from "@/hooks/use-current-user";
+
+import ExploreSection from "@/components/general/explore/explore-section";
+
 
 export default function Home() {
-
-  async function createPayment() {
-    createPaymentIntent(500, "usd").then(response => console.log(response)).catch(error => console.error(error))
-  }
-
-  const user = useCurrentUser()
-
   return (
-
-    <main className="flex flex-col items-start justify-start min-h-screen p-4 bg-[hsl(var(--background))] gap-4">
-      <button onClick={() => {createPayment()}}>
-        create payment intent
-      </button>
+    <main className="flex flex-col items-start justify-start min-h-screen p-4 bg-background gap-4">
+      <ExploreSection />
     </main>
   );
 }
