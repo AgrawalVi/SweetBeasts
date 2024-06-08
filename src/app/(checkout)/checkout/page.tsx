51ad8f82d768a0b3fa379d33e9a5092e31375c6f
Vishrut Agrawal <zvishrut@gmail.com>
2024-06-08 11:13:31 -0500

    goes to checkout page properly

--- src/app/(checkout)/checkout/page.tsx
+++ src/app/(checkout)/checkout/page.tsx
@@ -1,15 +1,33 @@
 'use client'
 
-import { Suspense } from 'react'
-import Checkout from '@/components/general/checkout/checkout'
+import { CustomCheckoutProvider } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+import { stripe_pk } from '@/constants'
+import CheckoutForm from '@/components/general/checkout/checkout-form'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useToast } from '@/components/ui/use-toast'
+
+const stripe = loadStripe(stripe_pk, {
+  betas: ['custom_checkout_beta_2'],
+})
 
 const CheckoutPage = () => {
-  // get client secret from search params, if no search params, then return error!
+  const searchParams = useSearchParams()
+  const { toast } = useToast()
+  const router = useRouter()
+
+  const clientSecret = searchParams.get('client_secret')
+
+  if (!clientSecret) {
+    toast({ description: 'Unable to start checkout', variant: 'destructive' })
+    router.replace('/products/pogo')
+    return null
+  }
 
   return (
-    <Suspense fallback="loading">
-      <Checkout />
-    </Suspense>
+    <CustomCheckoutProvider stripe={stripe} options={{ clientSecret }}>
+      <CheckoutForm />
+    </CustomCheckoutProvider>
   )
 }
 
