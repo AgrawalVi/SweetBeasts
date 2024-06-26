528e38c6612ee70cf5050820a511dfdf618d65f8
Vishrut Agrawal <zvishrut@gmail.com>
2024-06-27 01:55:47 +0200

    moved around files and configured react-query

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React from 'react'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+export default function RootClientLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
