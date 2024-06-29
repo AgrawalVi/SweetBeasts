285c074cdbf14c01d44db3274190850bb0aabd08
Vishrut Agrawal <zvishrut@gmail.com>
2024-06-29 17:39:23 -0500

    fixed layout to take full height

--- src/app/(root)/(floating-nav-pages)/layout.tsx
+++ src/app/(root)/(floating-nav-pages)/layout.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-full w-full">
+    <div className="flex h-full w-full flex-col">
       <FloatingNavbar />
       <div className="mt-20 h-full w-full">{children}</div>
     </div>
--- src/app/(root)/(floating-nav-pages)/page.tsx
+++ src/app/(root)/(floating-nav-pages)/page.tsx
@@ -5,11 +5,8 @@ import ExploreSection from '@/components/general/explore/explore-section'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-start justify-start gap-4 bg-background p-4">
+    <main className="flex flex-col items-start justify-start gap-4 bg-background p-4">
       <ExploreSection />
-      <div className="flex justify-center bg-background">
-        <ProductCards />
-      </div>
     </main>
   )
 }
