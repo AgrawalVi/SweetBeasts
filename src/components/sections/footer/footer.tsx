46b40c96c85ad78975696b2478b266e2265a840c
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-02 16:38:14 -0500

    changed padding for footer

--- src/components/sections/footer/footer.tsx
+++ src/components/sections/footer/footer.tsx
@@ -57,7 +57,7 @@ const footerLinks = {
 
 export default function Footer() {
   return (
-    <main className="flex w-full flex-col border-t border-muted p-5 font-nunito xl:pb-6 xl:pt-12">
+    <main className="flex w-full flex-col border-t border-muted p-12 pb-6 font-nunito">
       <div className="hidden w-full justify-center md:flex">
         <div className="grid w-full grid-cols-6">
           <div className="relative col-span-4 flex w-full flex-col items-center justify-center xl:col-span-3">
