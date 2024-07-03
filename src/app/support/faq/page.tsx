9ba86460444331044e179728852cca9e29e77127
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-02 21:50:23 -0500

    changed styling

--- src/app/support/faq/page.tsx
+++ src/app/support/faq/page.tsx
@@ -1,32 +1,33 @@
-import React from 'react';
+import React from 'react'
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion"
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+} from '@/components/ui/accordion'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default function FAQ() {
   return (
-    <div className="w-screen mx-auto px-4 py-8">
-      <h1 className="text-6xl font-bold text-center mb-4">Frequently Asked Questions</h1>
-      <p className="text-gray-600 text-2xl text-center mb-8">Here are the answers to the most frequently asked questions.</p>
+    <div className="mx-auto w-screen px-10 py-8">
+      <h1 className="mb-4 text-center text-6xl font-bold">
+        Frequently Asked Questions
+      </h1>
+      <p className="mb-8 text-center text-2xl text-gray-600">
+        Here are the answers to the most frequently asked questions.
+      </p>
 
       <div className="grid gap-8">
-        <Card className="w-full lg:w-4/5 xl:w-3/4 2xl:w-2/3 mx-auto">
+        <Card className="mx-auto w-full lg:w-4/5 xl:w-3/4 2xl:w-2/3">
           <CardHeader>
             <CardTitle>Most Frequently Asked Questions</CardTitle>
           </CardHeader>
           <CardContent>
             <Accordion type="single" collapsible>
               <AccordionItem value="item-1">
-                <AccordionTrigger>What materials are SweetBeasts plushies made from?</AccordionTrigger>
+                <AccordionTrigger>
+                  What materials are SweetBeasts plushies made from?
+                </AccordionTrigger>
                 <AccordionContent>
                   [Insert Here, idk what it is made of]
                 </AccordionContent>
@@ -34,61 +35,81 @@ export default function FAQ() {
             </Accordion>
           </CardContent>
         </Card>
-        <Card className="w-full lg:w-4/5 xl:w-3/4 2xl:w-2/3 mx-auto">
-          <CardHeader>
-            <CardTitle>Payments</CardTitle>
+        <Card className="mx-auto w-full lg:w-4/5 xl:w-3/4 2xl:w-2/3">
+          <CardHeader className="pb-0">
+            <CardTitle className="md:text-3xl">Payments</CardTitle>
           </CardHeader>
           <CardContent>
             <Accordion type="single" collapsible>
               <AccordionItem value="item-6">
-                <AccordionTrigger>Do you have a physical store where I can purchase SweetBeasts plushies?</AccordionTrigger>
-                <AccordionContent>
-                  At this time, SweetBeasts operates exclusively online. You can browse and purchase our plushies through our website, where we offer a secure and convenient shopping experience.
+                <AccordionTrigger className="text-start text-base md:text-lg">
+                  Do you have a physical store where I can purchase SweetBeasts
+                  plushies?
+                </AccordionTrigger>
+                <AccordionContent className="text-sm md:text-base">
+                  At this time, SweetBeasts operates exclusively online. You can
+                  browse and purchase our plushies through our website, where we
+                  offer a secure and convenient shopping experience.
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
           </CardContent>
         </Card>
-        <Card className="w-full lg:w-4/5 xl:w-3/4 2xl:w-2/3 mx-auto">
+        <Card className="mx-auto w-full lg:w-4/5 xl:w-3/4 2xl:w-2/3">
           <CardHeader>
             <CardTitle>Orders</CardTitle>
           </CardHeader>
           <CardContent>
             <Accordion type="single" collapsible>
               <AccordionItem value="item-6">
-                <AccordionTrigger>Do you have a physical store where I can purchase SweetBeasts plushies?</AccordionTrigger>
+                <AccordionTrigger>
+                  Do you have a physical store where I can purchase SweetBeasts
+                  plushies?
+                </AccordionTrigger>
                 <AccordionContent>
-                  At this time, SweetBeasts operates exclusively online. You can browse and purchase our plushies through our website, where we offer a secure and convenient shopping experience.
+                  At this time, SweetBeasts operates exclusively online. You can
+                  browse and purchase our plushies through our website, where we
+                  offer a secure and convenient shopping experience.
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
           </CardContent>
         </Card>
-        <Card className="w-full lg:w-4/5 xl:w-3/4 2xl:w-2/3 mx-auto">
+        <Card className="mx-auto w-full lg:w-4/5 xl:w-3/4 2xl:w-2/3">
           <CardHeader>
             <CardTitle>Shipping</CardTitle>
           </CardHeader>
           <CardContent>
             <Accordion type="single" collapsible>
               <AccordionItem value="item-6">
-                <AccordionTrigger>Do you have a physical store where I can purchase SweetBeasts plushies?</AccordionTrigger>
+                <AccordionTrigger>
+                  Do you have a physical store where I can purchase SweetBeasts
+                  plushies?
+                </AccordionTrigger>
                 <AccordionContent>
-                  At this time, SweetBeasts operates exclusively online. You can browse and purchase our plushies through our website, where we offer a secure and convenient shopping experience.
+                  At this time, SweetBeasts operates exclusively online. You can
+                  browse and purchase our plushies through our website, where we
+                  offer a secure and convenient shopping experience.
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
           </CardContent>
         </Card>
-        <Card className="w-full lg:w-4/5 xl:w-3/4 2xl:w-2/3 mx-auto">
+        <Card className="mx-auto w-full lg:w-4/5 xl:w-3/4 2xl:w-2/3">
           <CardHeader>
             <CardTitle>Returns</CardTitle>
           </CardHeader>
           <CardContent>
             <Accordion type="single" collapsible>
               <AccordionItem value="item-6">
-                <AccordionTrigger>Do you have a physical store where I can purchase SweetBeasts plushies?</AccordionTrigger>
+                <AccordionTrigger>
+                  Do you have a physical store where I can purchase SweetBeasts
+                  plushies?
+                </AccordionTrigger>
                 <AccordionContent>
-                  At this time, SweetBeasts operates exclusively online. You can browse and purchase our plushies through our website, where we offer a secure and convenient shopping experience.
+                  At this time, SweetBeasts operates exclusively online. You can
+                  browse and purchase our plushies through our website, where we
+                  offer a secure and convenient shopping experience.
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
@@ -96,5 +117,5 @@ export default function FAQ() {
         </Card>
       </div>
     </div>
-  );
+  )
 }
