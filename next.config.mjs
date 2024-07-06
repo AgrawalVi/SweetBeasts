49a8dd18306242d568f7affc87b38868cd50b33c
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-06 12:16:03 -0500

    added redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  redirects: async () => [
+    {
+      source: '/pogo',
+      destination: '/lore/pogo',
+      permanent: true,
+    },
+  ],
+}
 
 export default nextConfig
