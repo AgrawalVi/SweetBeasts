d9ebc6f8480f3c7f5de8850526eed255416547cb
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-09 21:41:34 -0500

    added more colors to order-status-bar.tsx

--- src/components/general/pages/account/order-status-badge.tsx
+++ src/components/general/pages/account/order-status-badge.tsx
@@ -5,8 +5,23 @@ import { OrderStatus } from '@prisma/client'
 export default function OrderStatusBadge({ status }: { status: OrderStatus }) {
   if (status === 'EXCEPTION') {
     return (
-      <Badge variant="destructive">
-        <span className="font-semibold">Exception</span>
+      <Badge variant="destructive" className="hover:bg-destructive">
+        <span className="font-semibold">{status}</span>
+      </Badge>
+    )
+  } else if (status === 'RETURNED' || status === 'CANCELLED') {
+    return (
+      <Badge variant="secondary" className="hover:bg-secondary">
+        <span className="font-semibold">{status}</span>
+      </Badge>
+    )
+  } else if (status === 'DELIVERED') {
+    return (
+      <Badge
+        variant="default"
+        className="bg-accent text-accent-foreground hover:bg-accent"
+      >
+        <span className="font-semibold">{status}</span>
       </Badge>
     )
   } else {
