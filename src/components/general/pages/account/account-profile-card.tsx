049a6e81c9bb081d7ef9e6b2db79e242e02cadd8
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-09 23:59:00 -0500

    Added logout button to account page

--- src/components/general/pages/account/account-profile-card.tsx
+++ src/components/general/pages/account/account-profile-card.tsx
@@ -6,10 +6,12 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { ChangeNameForm } from './profile-settings/change-name-form'
+import SignOutModalButton from '../../sign-out-modal-button'
 
 export default function MyProfileCard() {
   return (
-    <Card className="w-full sm:w-[30rem]">
+    <Card className="relative w-full sm:w-[30rem]">
+      <SignOutModalButton className="top-5 block big-phone:hidden" />
       <CardHeader>
         <CardTitle>Your Profile</CardTitle>
         <CardDescription>Manage your profile here</CardDescription>
