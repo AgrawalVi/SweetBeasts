2d0a787476debd47d43d08a888944ca34ef5308e
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-11 22:09:44 -0500

    initialized multi-step-loader in order success page

--- src/app/(root)/(full-nav-pages)/checkout-success/page.tsx
+++ src/app/(root)/(full-nav-pages)/checkout-success/page.tsx
@@ -5,6 +5,7 @@ import { getOrderWithDataByStripeSessionid } from '@/data/shop/orders'
 import { OrderWithData } from '@/types'
 import { formatDate } from '@/lib/date-functions'
 import Link from 'next/link'
+import { MultiStepLoaderDemo } from '@/components/general/pages/checkout-success/multi-step-loader'
 
 export default async function CheckoutSuccess({
   searchParams,
@@ -46,38 +47,41 @@ export default async function CheckoutSuccess({
   const lineItems = order.lineItems
 
   return (
-    <main className="flex w-full flex-col items-center justify-center space-y-8">
-      <div className="mx-5 flex w-full max-w-6xl flex-col items-center justify-between sm:flex-row">
-        <div className="header-gradient text-center text-5xl sm:text-left">
-          Thank You For Your Order!
+    <>
+      <MultiStepLoaderDemo />
+      <main className="flex w-full flex-col items-center justify-center space-y-8">
+        <div className="mx-5 flex w-full max-w-6xl flex-col items-center justify-between sm:flex-row">
+          <div className="header-gradient text-center text-5xl sm:text-left">
+            Thank You For Your Order!
+          </div>
+          <div className="flex flex-col text-center text-sm text-muted-foreground sm:text-end sm:text-base">
+            <div>Order {order.orderNumber}</div>
+            <div>Placed {formatDate(order.createdAt)}</div>
+          </div>
         </div>
-        <div className="flex flex-col text-center text-sm text-muted-foreground sm:text-end sm:text-base">
-          <div>Order {order.orderNumber}</div>
-          <div>Placed {formatDate(order.createdAt)}</div>
+        <div className="h-full max-w-6xl flex-col items-start justify-start space-y-4 xl:grid xl:grid-cols-2 xl:gap-4 xl:space-y-0">
+          <div className="flex w-full">
+            {lineItems && lineItems.length > 0 ? (
+              <OrderSummary orderItems={lineItems} order={order} />
+            ) : (
+              <div>No items found in your order</div>
+            )}
+          </div>
+          <div className="flex w-full flex-col space-y-4 xl:items-start xl:justify-center">
+            <OrderStatusBar order={order} />
+            <OrderDetails order={order} />
+          </div>
         </div>
-      </div>
-      <div className="h-full max-w-6xl flex-col items-start justify-start space-y-4 xl:grid xl:grid-cols-2 xl:gap-4 xl:space-y-0">
-        <div className="flex w-full">
-          {lineItems && lineItems.length > 0 ? (
-            <OrderSummary orderItems={lineItems} order={order} />
-          ) : (
-            <div>No items found in your order</div>
-          )}
+        <div>
+          Have an issue?{' '}
+          <Link
+            href="/support/contact-us"
+            className="underline underline-offset-2"
+          >
+            Contact us
+          </Link>
         </div>
-        <div className="flex w-full flex-col space-y-4 xl:items-start xl:justify-center">
-          <OrderStatusBar order={order} />
-          <OrderDetails order={order} />
-        </div>
-      </div>
-      <div>
-        Have an issue?{' '}
-        <Link
-          href="/support/contact-us"
-          className="underline underline-offset-2"
-        >
-          Contact us
-        </Link>
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
---
+++ src/components/general/pages/checkout-success/multi-step-loader.tsx
@@ -0,0 +1,57 @@
+'use client'
+import React, { useState } from 'react'
+import { MultiStepLoader as Loader } from '@/components/aceternity/multi-step-loader'
+import { IconSquareRoundedX } from '@tabler/icons-react'
+
+const loadingStates = [
+  {
+    text: 'Buying a condo',
+  },
+  {
+    text: 'Travelling in a flight',
+  },
+  {
+    text: 'Meeting Tyler Durden',
+  },
+  {
+    text: 'He makes soap',
+  },
+  {
+    text: 'We goto a bar',
+  },
+  {
+    text: 'Start a fight',
+  },
+  {
+    text: 'We like it',
+  },
+  {
+    text: 'Welcome to F**** C***',
+  },
+]
+
+export function MultiStepLoaderDemo() {
+  const [loading, setLoading] = useState(true)
+
+  return (
+    <>
+      {/* Core Loader Modal */}
+      <Loader
+        loadingStates={loadingStates}
+        loading={loading}
+        setLoading={setLoading}
+        duration={1000}
+        loop={false}
+      />
+
+      {loading && (
+        <button
+          className="fixed right-4 top-4 z-[120] text-black dark:text-white"
+          onClick={() => setLoading(false)}
+        >
+          <IconSquareRoundedX className="h-10 w-10" />
+        </button>
+      )}
+    </>
+  )
+}
