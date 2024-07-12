f78db014f3f1ddbc2c97c0455e8fd9e9465fe5d3
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-11 23:28:41 -0500

    fixed introducing pogo text

--- src/components/general/pages/home/heros/introducting-product.tsx
+++ src/components/general/pages/home/heros/introducting-product.tsx
@@ -24,20 +24,20 @@ export default function IntroducingProduct({
         <CardBody className="group/card relative flex h-full w-full flex-col items-center justify-center rounded-none border-none">
           <CardItem
             translateZ="30"
-            className="header-gradient text-center text-[2.5rem] text-transparent transition-all big-phone:text-5xl sm:text-7xl"
+            className="header-gradient sm:text-56xl text-center text-[2.5rem] text-transparent transition-all big-phone:text-6xl"
           >
             Introducing
           </CardItem>
           <CardItem
             translateZ="30"
-            className="header-gradient text-center text-8xl text-transparent transition-all big-phone:text-9xl sm:text-[12rem] md:text-[15rem]"
+            className="header-gradient text-center text-8xl text-transparent transition-all big-phone:text-9xl sm:text-9xl 3xl:text-[10rem]"
           >
             Pogo
           </CardItem>
           <CardItem
             as="p"
             translateZ="30"
-            className="header-gradient mb-4 mt-2 max-w-2xl text-center text-2xl text-transparent transition-all big-phone:text-3xl sm:text-5xl md:text-6xl"
+            className="header-gradient mb-4 mt-2 max-w-2xl text-center text-2xl text-transparent transition-all big-phone:text-3xl sm:text-4xl md:text-6xl"
           >
             The Peachy Penguin
           </CardItem>
