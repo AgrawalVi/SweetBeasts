ea8f9367af2c289ed1d19ad3d7300f6dcef91ca0
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-13 01:29:35 -0500

    made adjustments

--- src/app/(root)/(full-nav-pages)/(support)/feedback-form/page.tsx
+++ src/app/(root)/(full-nav-pages)/(support)/feedback-form/page.tsx
@@ -1,13 +1,21 @@
-import FeedbackForm from "@/components/general/feedback/form";
+import FeedbackForm from '@/components/general/feedback/form'
 
 export default function FeedbackFormPage() {
   return (
     <div>
-      <h1 className="text-center text-6xl text-bold header-gradient">Feedback Form</h1>
-      <h3 className="text-center text-3xl text-semibold h2-gradient text-indigo-900 dark:text-purple-300">Have any Suggestions?</h3>
-      <h4 className="text-center text-2xl text-semibold h2-gradient text-indigo-900 dark:text-purple-300">Fill out the Form below!</h4>
-      <p className="text-center text-xl text-semibold p-gradient">We will get back to you as soon as possible!</p>
+      <h1 className="text-bold header-gradient text-center text-6xl">
+        Feedback Form
+      </h1>
+      <h3 className="text-semibold h2-gradient text-center text-3xl text-indigo-900 dark:text-purple-300">
+        Have any Suggestions?
+      </h3>
+      <h4 className="text-semibold h2-gradient text-center text-2xl text-indigo-900 dark:text-purple-300">
+        Fill out the Form below!
+      </h4>
+      <p className="text-semibold p-gradient text-center text-xl">
+        We will get back to you as soon as possible!
+      </p>
       <FeedbackForm />
     </div>
-  );
+  )
 }
