f93e660fefd498de195b35f7f03933acd08b4833
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-12 23:10:59 -0500

    added logic to update searchParams when quantity is adjusted

--- src/components/general/pages/product/add-to-cart-quantity/add-to-cart-quantity-secton.tsx
+++ src/components/general/pages/product/add-to-cart-quantity/add-to-cart-quantity-secton.tsx
@@ -1,22 +1,36 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AddToCartButton from './add-to-cart-button'
 import QuantitySelector from './quantity-selector'
+import { useRouter } from 'next/navigation'
 
 interface AddToCartWithQuantitySectionProps {
   productId: number
   priceInCents: number
+  initialQuantity: number
+  className?: string
 }
 
 export default function AddToCartWithQuantitySection({
   productId,
   priceInCents,
+  className,
+  initialQuantity = 1,
 }: AddToCartWithQuantitySectionProps) {
-  const [quantity, setQuantity] = useState<number>(1)
+  const [quantity, setQuantity] = useState<number>(initialQuantity)
+  const router = useRouter()
+
+  useEffect(() => {
+    if (quantity > 1) {
+      router.replace(`/products/pogo?quantity=${quantity}`)
+    } else {
+      router.replace(`/products/pogo`)
+    }
+  }, [quantity])
 
   return (
-    <main>
+    <main className="flex space-x-4">
       <AddToCartButton
         productId={productId}
         priceInCents={priceInCents}
