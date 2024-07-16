4ca1df4923e77e2bff6da5700a733f188bfa4145
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-15 19:40:48 -0500

    renamed component

--- src/components/general/pages/home/heros/home-product-hero.tsx
+++ src/components/general/pages/home/heros/home-product-hero.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button'
-import ExploreSection from './introducting-product'
+import IntroducingProduct from './introducting-product'
 import Link from 'next/link'
 import { WobbleButton } from '@/components/custom/wobble-button'
 import Image from 'next/image'
@@ -8,7 +8,7 @@ export default function HomeProductHero() {
   return (
     <div className="flex h-screen w-full items-center justify-center bg-[url('/home-page.jpg')] bg-cover bg-center lg:h-[70vh]">
       <div>
-        <ExploreSection />
+        <IntroducingProduct />
         <WobbleButton className="relative z-20">
           <Link href="/products/pogo">Shop Now</Link>
         </WobbleButton>
