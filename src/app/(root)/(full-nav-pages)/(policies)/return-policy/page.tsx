16fd3fa4ee53092ff23cd46c7f9606c0749cd9dd
Vishrut Agrawal <zvishrut@gmail.com>
2024-07-15 20:08:09 -0500

    changed format of return policy page

--- src/app/(root)/(full-nav-pages)/(policies)/return-policy/page.tsx
+++ src/app/(root)/(full-nav-pages)/(policies)/return-policy/page.tsx
@@ -1,15 +1,10 @@
 'use client'
 import React from 'react'
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default function ReturnPolicy() {
   return (
-    <Card className="mx-auto flex min-h-[70vh] max-w-4xl flex-col px-4 py-8 mb-24">
+    <Card className="mx-auto mb-24 flex max-w-4xl flex-col px-4 py-8">
       <CardHeader>
         <CardTitle className="header-gradient text-center text-6xl">
           Return Policy
@@ -19,37 +14,49 @@ export default function ReturnPolicy() {
         <div className="text-left">
           <p className="mb-4 text-lg">
             Hello and thank you for choosing SweetBeasts. We want you to be
-            delighted with your purchase, but we understand that sometimes things
-            don't work out as expected. That's why we've made our return process
-            as easy as possible.
+            delighted with your purchase, but we understand that sometimes
+            things don't work out as expected. That's why we've made our return
+            process as easy as possible.
           </p>
 
-          <h2 className="p-gradient mb-4 text-2xl font-semibold">Returns</h2>
+          <h2 className="p-gradient mb-4 text-center text-3xl font-semibold">
+            Returns
+          </h2>
           <p className="mb-4">
             You can return your plushie within 2 weeks (14 days) of delivery for
             any reason, as long as it's in its original condition with the tags
             still attached.
           </p>
 
-          <h2 className="p-gradient mb-4 text-2xl font-semibold">Refunds</h2>
+          <h2 className="p-gradient mb-4 text-center text-3xl font-semibold">
+            Refunds
+          </h2>
           <p className="mb-4">
             Once we receive your returned plushie and confirm its condition, we
             will initiate a refund to your original method of payment. Please
             allow 5-7 business days for the refund to appear in your account.
           </p>
 
-          <h2 className="p-gradient mb-4 text-2xl font-semibold">Shipping</h2>
+          <h2 className="p-gradient mb-4 text-center text-3xl font-semibold">
+            Shipping
+          </h2>
           <p className="mb-4">
             You will be responsible for paying for your own shipping costs for
-            returning your plushie. Unfortunately, original shipping and handling
-            charges are non-refundable.
+            returning your plushie. Unfortunately, original shipping and
+            handling charges are non-refundable.
           </p>
 
-          <h2 className="p-gradient mb-4 text-2xl font-semibold">Contact Us</h2>
+          <h2 className="p-gradient mb-4 text-center text-3xl font-semibold">
+            Contact Us
+          </h2>
           <p>
-            If you have any questions about our return process or need assistance
-            with a return, please reach out to our customer service team at{' '}
-            <a href="mailto:support@sweetbeasts.shop" className="text-purple-500">
+            If you have any questions about our return process or need
+            assistance with a return, please reach out to our customer service
+            team at{' '}
+            <a
+              href="mailto:support@sweetbeasts.shop"
+              className="text-purple-500"
+            >
               support@sweetbeasts.shop
             </a>
             .
@@ -59,4 +66,3 @@ export default function ReturnPolicy() {
     </Card>
   )
 }
-
