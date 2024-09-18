4531d9b35e5b3e8aacabfdf6a38a388db228da83
Pranav Gogineni <pranavgv@gmail.com>
2024-09-17 23:35:16 -0500

    added a lore page for pogo for the learn more

---
+++ src/app/(root)/(floating-nav-pages)/lore/pogo/page.tsx
@@ -0,0 +1,31 @@
+import TextSection from '@/components/custom/text-section'
+import JoinEmailListForm from '@/components/sections/footer/email-list-form'
+
+export default function HomeSweetbeastsMission() {
+  return (
+    <div className="relative flex w-full flex-col items-center justify-center py-20">
+      <div className="max-w-6xl xl:grid xl:grid-cols-3">
+        <div className="col-span-3">
+          <div className="header-gradient md:text-upright flex w-full flex-col justify-center text-center text-4xl sm:text-5xl md:text-7xl">
+            Pogo's Story
+          </div>
+        </div>
+        <div className="col-span-3">
+          <TextSection text="Living a life in Africa, where everything's just a waddle away, Pogo has never had to work hard for anything. Spoiled with riches, but poor in understanding, Pogo struggles to see beyond her luxurious hot tub. Her world is simple: if you want it, you buy it.
+
+One seemingly ordinary day, her butler presented her with her daily bowl of fruit. Among the fruit, a peculiar peach caught Pogo’s eye—it smelled funny and looked odd, but curiosity got the better of her. Swallowing it whole, she was sent into a deep slumber, tumbling down into the colorful, vibrant world of Sweet Haven.
+
+Upon awakening in Sweet Haven, Pogo discovered she had transformed into a Peach Penguin on Pitfall Shores! She comes to realize slowly that no one cares to listen to her commands or tantrums. As her exploration outside her life of privilege continues, she embarks on a journey of becoming more open-minded, making heartfelt connections along the way. What lies in store?
+
+
+ 
+
+As she explores this new world, she comes to realize that  she was just like everyone else–no special treatment, no high status. For the first time in her life, Pogo was living a life not lined with privilege, setting the stage for a journey of real understanding and heartfelt connections. 
+
+Her exploration through this new world continues, outside her life of privilege, sets the stage for a journey of learning compassion and making heartfelt connections. What lies in store?
+" />
+        </div>
+      </div>
+    </div>
+  )
+}
