9089a09919f5eec07d67cd3287cedbed8c241e1b
Vishrut Agrawal <zvishrut@gmail.com>
2024-09-23 15:00:11 -0500

    Added sweet-haven pages

---
+++ src/app/(root)/(floating-nav-pages)/sweet-haven/pogo/page.tsx
@@ -0,0 +1,9 @@
+export default function PogoLorePage() {
+  return (
+    <main className="pt-10">
+      <div className="header-gradient text-center text-5xl sm:text-6xl md:text-7xl">
+        SweetHaven
+      </div>
+    </main>
+  )
+}
