02b19269e2bf7c051fab4f03daa29a744bc1bc29
Vishrut Agrawal <zvishrut@gmail.com>
2024-09-23 23:10:47 -0500

    Extracted lorecard component

--- src/app/(root)/(floating-nav-pages)/sweet-haven/page.tsx
+++ src/app/(root)/(floating-nav-pages)/sweet-haven/page.tsx
@@ -1,36 +1,22 @@
 import TextSection from '@/components/custom/text-section'
-import { BackgroundGradient } from '@/components/aceternity/background-gradient'
-import Image from 'next/image'
 import React from 'react'
-import Link from 'next/link'
+import LoreCard from '@/components/general/sweet-haven/lore-card'
 
 export default function PogoLorePage() {
   return (
-    <main className="pt-10">
+    <main className="flex flex-col items-center pt-10">
       <div className="header-gradient text-center text-5xl sm:text-6xl md:text-7xl">
         Sweet Haven
       </div>
-      <TextSection text="Hello" />
-      <Link href={'/sweet-haven/pogo'}>
-        <BackgroundGradient className="rounded-lg bg-white p-4 dark:bg-zinc-900 sm:p-10">
-          <div className="group relative h-[300px] w-[300px]">
-            <Image
-              src={`/plushie-art/pogo/pogo-animation-static.png`}
-              alt="Pogo Animation Static"
-              height="300"
-              width="300"
-              className="absolute left-0 top-0 object-contain transition-opacity duration-300 group-hover:opacity-0"
-            />
-            <Image
-              src={`/plushie-art/pogo/pogo-animation.GIF`}
-              alt="Pogo Animation GIF"
-              height="300"
-              width="300"
-              className="absolute left-0 top-0 object-contain opacity-0 transition-opacity duration-300 group-hover:opacity-100"
-            />
-          </div>
-        </BackgroundGradient>
-      </Link>
+      <TextSection className="" text="Hello" />
+      <div className="flex flex-wrap justify-center gap-4 sm:gap-8">
+        <LoreCard
+          staticImageHref="/plushie-art/pogo/pogo-animation-static.png"
+          animatedImageHref="/plushie-art/pogo/pogo-animation.GIF"
+          productName="Pogo"
+          productDescription="The Peachy Penguin"
+        />
+      </div>
     </main>
   )
 }
---
+++ src/components/general/sweet-haven/lore-card.tsx
@@ -0,0 +1,46 @@
+import { BackgroundGradient } from '@/components/aceternity/background-gradient'
+import Image from 'next/image'
+import Link from 'next/link'
+
+interface LoreCardProps {
+  staticImageHref: string
+  animatedImageHref: string
+  productName: string
+  productDescription: string
+}
+
+export default function LoreCard({
+  staticImageHref,
+  animatedImageHref,
+  productName,
+  productDescription,
+}: LoreCardProps) {
+  return (
+    <Link href={`/sweet-haven/${productName.toLowerCase()}`}>
+      <BackgroundGradient className="rounded-lg bg-white p-4 dark:bg-zinc-900 sm:p-10">
+        <h1 className="header-gradient text-center font-coiny text-4xl">
+          {productName}
+        </h1>
+        <p className="p-gradient text-center text-xl">
+          Read about {productDescription}
+        </p>
+        <div className="group relative h-[300px] w-[300px]">
+          <Image
+            src={staticImageHref}
+            alt={`${productName} Animation Static`}
+            height="300"
+            width="300"
+            className="absolute left-0 top-0 object-contain transition-opacity duration-300 group-hover:opacity-0"
+          />
+          <Image
+            src={animatedImageHref}
+            alt={`${productName} Animation`}
+            height="300"
+            width="300"
+            className="absolute left-0 top-0 object-contain opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+          />
+        </div>
+      </BackgroundGradient>
+    </Link>
+  )
+}
