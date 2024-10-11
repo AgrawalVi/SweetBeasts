5a03ef79a15990a56b2ec1e39aef9ef5905be1d0
Vishrut Agrawal <zvishrut@gmail.com>
2024-10-11 18:40:24 -0500

    changed pogo lore

--- src/components/general/pages/product/lore-teaser.tsx
+++ src/components/general/pages/product/lore-teaser.tsx
@@ -8,16 +8,12 @@ export default function LoreTeaser() {
       </div>
       <div>
         <TextSection
-          text="Living a life in Africa, where everything's just a waddle away, Pogo has never had to work hard for anything. Spoiled with riches, but poor in understanding, Pogo struggles to see beyond her luxurious hot tub. Her world is simple: if you want it, you buy it.
-
-One seemingly ordinary day, her butler presented her with her daily bowl of fruit. Among the fruit, a peculiar peach caught Pogo’s eye—it smelled funny and looked odd, but curiosity got the better of her. Swallowing it whole, she was sent into a deep slumber, tumbling down into the colorful, vibrant world of Sweet Haven.
-
-Upon awakening in Sweet Haven, Pogo discovered she had transformed into a Peach Penguin on Pitfall Shores! She comes to realize slowly that no one cares to listen to her commands or tantrums. As her exploration outside her life of privilege continues, she embarks on a journey of becoming more open-minded, making heartfelt connections along the way. What lies in store?
-"
+          text="Pogo, once spoiled and carefree, is thrust into the vibrant world of Sweet Haven after eating a mysterious peach. Now transformed into a Peach Penguin, she embarks on a journey of self-discovery and growth, learning that true richness may instead lie in heartfelt connections."
           linkInfo={{
             linkText: 'Learn about Pogo',
             linkHref: '/lore/pogo',
           }}
+          textClassName="text-xl"
         />
       </div>
     </div>
