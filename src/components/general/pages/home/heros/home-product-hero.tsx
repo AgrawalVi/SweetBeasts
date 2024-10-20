567a06d491965935b2be144a092f28eb49bbd3ce
Vishrut Agrawal <zvishrut@gmail.com>
2024-10-20 13:51:16 -0500

    Completed Home Page + moved around assets in public folder

--- src/components/general/pages/home/heros/home-product-hero.tsx
+++ src/components/general/pages/home/heros/home-product-hero.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { WobbleButton } from '@/components/custom/wobble-button'
@@ -6,12 +7,20 @@ import IntroducingProduct from './introducting-product'
 
 export default function HomeProductHero() {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-[url('/home-page.webp')] bg-cover bg-center lg:h-[70vh] z-20">
-      <div>
-        <IntroducingProduct />
-        <WobbleButton className="relative">
-          <Link href="/products/pogo">Shop Now</Link>
-        </WobbleButton>
+    <div className="flex h-dvh w-full relative items-center justify-center lg:h-[95vh] z-20 transition-all">
+      <Image
+        src="/home-page/pogo/pogo-home.png"
+        alt="Background"
+        fill
+        className="absolute top-0 left-0 w-full h-full object-cover object-bottom z-10"
+      />
+      <div className="z-20">
+        <div className="lg:left-1/2 lg:top-32 transform lg:-translate-x-1/2 lg:absolute -translate-y-12 lg:translate-y-0 transition-transform">
+          <IntroducingProduct />
+          <WobbleButton className="relative">
+            <Link href="/products/pogo">Shop Now</Link>
+          </WobbleButton>
+        </div>
       </div>
     </div>
   )
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -14,4 +14,8 @@ export const DISCORD_URL = 'https://discord.gg/sweetbeasts'
 export const TIKTOK_URL = 'https://www.tiktok.com/@sweetbeasts'
 export const SUPPORT_EMAIL_URL = 'mailto:support@sweetbeasts.shop'
 
+export const TIKTOK_LOGO = 'https://sweetbeasts.shop/logos/tiktok-logo.png'
+export const DISCORD_LOGO = 'https://sweetbeasts.shop/logos/discord-logo.png'
+export const INSTAGRAM_LOGO = 'https://sweetbeasts.shop/logos/instagram-logo.png'
+
 export const LOGO_PNG_URL = 'https://sweetbeasts.shop/sweetbeasts-logo.png'
